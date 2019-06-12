43ea5959eda7c069d94baf0541806b3ac4d973fa
ArchitectGuan <34328916+ArchitectGuan@users.noreply.github.com>
2019-06-13 00:55:41 +0800

    Add a map to form-section
    Add a map to the form section and fix the problem that the marker not in the center of the map

--- Jonas's Courses/omnifood/resources/js/scripts.js
+++ Jonas's Courses/omnifood/resources/js/scripts.js
@@ -1,5 +1,4 @@
 $(document).ready(() => {
-
     // For sticky nav
     $('.js--section-features').waypoint(function (direction) {
         direction === 'down' ? $('nav').addClass('sticky') : $('nav').removeClass('sticky');
@@ -80,4 +79,43 @@ $(document).ready(() => {
             icon.addClass('ion-navicon');
         }
     });
+
+
+    // All for the map
+    var url = 'https://webapi.amap.com/maps?v=1.4.15&key=AIzaSyB9ikPWbIzMnKLBTUMC-k3dddagVUMF3zg&callback=onLoad';
+    var jsapi = document.createElement('script');
+    jsapi.charset = 'utf-8';
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
 });
+
+// For the map in the form section
+window.onLoad = () => {
+    
+    //为了控制Marker在地图初始化时的位置显示在地图中央附近
+    let width = window.innerWidth;
+    let coor;
+    if (width < 767)
+        coor = [113.3864049, 23.0376927];
+    else if (width < 1028)
+        coor = [113.466047, 23.029700];
+    else if (width < 1110)
+        coor = [113.486047, 23.029700];
+    else
+        coor = [113.506047, 23.029700];
+
+    var map = new AMap.Map('map', {
+        center: coor,                                                   // x + <- 
+        zoom: 12
+    });
+
+    var marker = new AMap.Marker({
+        position: [113.387876, 23.037911],
+        offset: new AMap.Pixel(-12, -12),
+        zIndex: 101,
+        title: 'Our Guangzhou HQ is here!',
+        map: map
+    });
+
+}
+
